436a233a519bc7413dbd1a2dc4c2a71b8c98b475
AnsonChuang-TW <ansonchuangtw@gmail.com>
2025-06-05 06:14:13 +0800

    fix error

---
+++ backend/src/main/java/com/project2/product/service/TestMaven.java
@@ -0,0 +1,10 @@
+package com.project2.product.service;
+
+public class TestMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TEST");
+	}
+
+}
