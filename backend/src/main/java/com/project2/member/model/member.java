51387b4f0ba570f5c43b7cda2b6048e4d6079ff3
wang-tze <ztaniose@gmail.com>
2025-06-05 13:06:41 +0800

    0605model

---
+++ backend/src/main/java/com/project2/member/model/member.java
@@ -0,0 +1,13 @@
+package com.project2.member.model;
+
+public class member {
+	private Integer id;
+	private String memberName;
+	private String memberGender;
+	private String memberPhone;
+	private String memberIDCard;
+	private String memberAddress;
+	private String memberEmail;
+	private String memberBD;
+	private String memberAccess;
+}
