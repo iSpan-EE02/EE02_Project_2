67f379ebef7f2c6cd57e7971a8305f52b25403a0
ylHsiung <ylhsiung776@gmail.com>
2025-06-10 18:06:28 +0800

    add

--- sql/36/spec/funding.sql
+++ sql/36/spec/funding.sql
@@ -1,20 +1,19 @@
 CREATE TABLE fundraising_campaigns (
-    campaign_id INT PRIMARY KEY IDENTITY(1,1), -- PK: 募資活動編號
-    
-    name VARCHAR(200) NOT NULL,         -- 活動名稱
-    category VARCHAR(100) NOT NULL,     -- 類別
+    campaign_id INT PRIMARY KEY IDENTITY(1,1), --  募資活動編號
+    campaign_name VARCHAR(200) NOT NULL,         -- 活動名稱
+    campaign_category VARCHAR(100) NOT NULL,     -- 類別
     goal_amount DECIMAL(10, 2) NOT NULL,-- 目標金額
     current_amount DECIMAL(10, 2) DEFAULT 0, -- 目前金額
     start_date DATE NOT NULL,           
     end_date DATE NOT NULL,             
     cover_image VARCHAR(255),           
     status VARCHAR(20) DEFAULT '草稿', -- 草稿、進行中、成功、失敗、已結束
-
-    creator_id INT NOT NULL, -- FK: 對應 members.member_id
-
-   -- FOREIGN KEY (creator_id) REFERENCES members(member_id)
+	creator_id INT NOT NULL, -- FK: 對應 members.member_id
+	approval_status VARCHAR(20) DEFAULT '待審核',       -- 審核狀態（待審核、通過、未通過）
+   -- FOREIGN KEY (creator_id) REFERENCES mem(memid)
 );
 
+DROP TABLE fundraising_campaigns;
 
 CREATE TABLE funding_orders (
     order_id INT PRIMARY KEY IDENTITY(1,1), -- PK: 訂單編號
@@ -28,5 +27,5 @@ CREATE TABLE funding_orders (
     payment_status VARCHAR(20) DEFAULT '未付款', -- 付款狀態：未付款、已付款、退款
 
    -- FOREIGN KEY (campaign_id) REFERENCES fundraising_campaigns(campaign_id),
-    --FOREIGN KEY (member_id) REFERENCES members(member_id)
+    --FOREIGN KEY (member_id) REFERENCES mem(memid)
 );
