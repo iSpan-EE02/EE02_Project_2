1b29b2a1371bb27b9f6db652208bb78779fcc307
AnsonChuang-TW <ansonchuangtw@gmail.com>
2025-06-23 16:49:38 +0800

    remove front-end folder
    remove front-end folder,move all front-end assets to /webapp

---
+++ backend/src/main/java/product/controller/DaoQueryAll.java
@@ -0,0 +1,58 @@
+package product.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import product.bean.EmpBean;
+
+import product.dao.EmpDao;
+
+/**
+ * Servlet implementation class DaoQueryAll
+ */
+@WebServlet("/DaoQueryAll")
+public class DaoQueryAll extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DaoQueryAll() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		List<EmpBean> empList = null;
+		EmpDao empDao = new EmpDao();
+		try {
+			empList = empDao.queryAllEmp();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		request.setAttribute("emps", empList);
+		request.getRequestDispatcher("/Page").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ backend/src/main/java/product/dao/EmpDao.java
@@ -0,0 +1,161 @@
+package product.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+/**This class is a collection of DAO method to access EmpBean
+ * 
+ * 
+ * @Author:Anson_Chuang
+ * @version 1.0
+ * @since 2025-06-17
+ */
+
+import product.bean.EmpBean;
+import product.util.JDBCutil;
+
+public class EmpDao {
+//	private static final String sqlInsert = "INSERT INTO employee(empno,ename,"
+//			+ "hiredate,salary,deptno,title) VALUES(" + "?,?,?,?,?,?)";
+//
+//	private static final String sqlDelete = "DELETE FROM employee WHERE empno = ?";
+//
+//	private static final String sqlUpdate = "UPDATE employee " + "SET ename = ?," + "hiredate = ?," + "salary = ?,"
+//			+ "deptno = ?," + "title = ?" + "WHERE empno = ?";
+
+	private static final String sqlQueryAll = "select e.empno,e.ename,e.hiredate,e.salary,e.deptno,e.title,d.dname from employee e Join department d on e.deptno = d.deptno;";
+//
+//	private static final String sqlQueryOne = "select * from employee where empno = ?";
+//	
+//	private static final String sqlQueryAllByPart = "select e.empno,e.ename,e.hiredate,e.salary,e.deptno,e.title,d.dname from employee e Join department d on e.deptno = d.deptno where e.ename Like ? and e.deptno = ?";
+//
+//	/*
+//	 * This is an insert DAO method of EmpBean
+//	 * 
+//	 */
+//	public void insertEmp(EmpBean emp) throws SQLException {
+//		Connection conn = JDBCutil.getConnection();
+//		PreparedStatement ps = conn.prepareStatement(sqlInsert);
+//		ps.setInt(1, Integer.parseInt(emp.getEmpno()));
+//		ps.setString(2, emp.getEname());
+//		ps.setDate(3, java.sql.Date.valueOf(emp.getHiredate()));
+//		ps.setInt(4, Integer.parseInt(emp.getSalary()));
+//		ps.setString(5, emp.getDeptno());
+//		ps.setString(6, emp.getTitle());
+//		ps.execute();
+//		JDBCutil.closeResource(ps);
+//	}
+//
+//	/*
+//	 * This is a delete DAO method of EmpBean
+//	 * 
+//	 */
+//	public void deleteEmp(String empno) throws SQLException {
+//		Connection conn = JDBCutil.getConnection();
+//		PreparedStatement stmt = conn.prepareStatement(sqlDelete);
+//		stmt.setString(1, empno);
+//		stmt.execute();
+//		JDBCutil.closeResource(stmt);
+//	}
+//
+//	/*
+//	 * This is a update DAO method of EmpBean
+//	 * 
+//	 */
+//	public void updateEmp(EmpBean emp) throws SQLException {
+//		Connection conn = JDBCutil.getConnection();
+//		PreparedStatement ps = conn.prepareStatement(sqlUpdate);
+//		ps.setInt(6, Integer.parseInt(emp.getEmpno()));
+//		ps.setString(1, emp.getEname());
+//		ps.setDate(2, java.sql.Date.valueOf(emp.getHiredate()));
+//		ps.setInt(3, Integer.parseInt(emp.getSalary()));
+//		ps.setString(4, emp.getDeptno());
+//		ps.setString(5, emp.getTitle());
+//		ps.execute();
+//		JDBCutil.closeResource(ps);
+//	}
+//
+//	/*
+//	 * This is a query single DAO method of EmpBean
+//	 * 
+//	 */
+//	public EmpBean queryOneEmp(String empno) throws SQLException {
+//		EmpBean emp = new EmpBean();
+//		Connection conn = JDBCutil.getConnection();
+//		PreparedStatement stmt = conn.prepareStatement(sqlQueryOne);
+//		stmt.setString(1, empno);
+//		ResultSet rs = stmt.executeQuery();
+//		if (rs.next()) {
+//			emp.setEmpno(rs.getString("empno"));
+//			emp.setEname(rs.getString("ename"));
+//			emp.setHiredate(rs.getString("hiredate"));
+//			emp.setSalary(rs.getString("salary"));
+//			emp.setDeptno(rs.getString("deptno"));
+//			emp.setTitle(rs.getString("title"));
+//		}
+//		JDBCutil.closeResource(stmt, rs);
+//		return emp;
+//	}
+
+	/*
+	 * This is a query All DAO method of EmpBean
+	 * 
+	 */
+	public List<EmpBean> queryAllEmp() throws SQLException {
+		List<EmpBean> empList = new ArrayList<EmpBean>();
+		// empList.add
+		Connection conn = JDBCutil.getConnection();
+		PreparedStatement stmt = conn.prepareStatement(sqlQueryAll);
+		ResultSet rs = stmt.executeQuery();
+		EmpBean emp = null;
+		while (rs.next()) {
+			emp = new EmpBean();
+			emp.setEmpno(rs.getString("empno"));
+			emp.setEname(rs.getString("ename"));
+			emp.setHiredate(rs.getString("hiredate"));
+			emp.setSalary(rs.getString("salary"));
+			emp.setDeptno(rs.getString("deptno"));
+			emp.setTitle(rs.getString("title"));
+			emp.setDname(rs.getString("dname"));
+			empList.add(emp);
+		}
+		JDBCutil.closeResource(stmt, rs);
+		return empList;
+	}
+	
+//	/*
+//	 * This is a query All DAO method of EmpBean by part of name
+//	 * 
+//	 */
+//	public List<EmpBean> queryAllEmpByPart(String enamePart, String deptno) throws SQLException {
+//		List<EmpBean> empList = new ArrayList<EmpBean>();
+//		// empList.add
+//		Connection conn = JDBCutil.getConnection();
+//		PreparedStatement stmt = conn.prepareStatement(sqlQueryAllByPart);
+//		stmt.setString(1, enamePart + "%");
+//		stmt.setInt(2, Integer.parseInt(deptno.trim()));
+//		ResultSet rs = stmt.executeQuery();
+//		EmpBean emp = null;
+//		while (rs.next()) {
+//			emp = new EmpBean();
+//			emp.setEmpno(rs.getString("empno"));
+//			emp.setEname(rs.getString("ename"));
+//			emp.setHiredate(rs.getString("hiredate"));
+//			emp.setSalary(rs.getString("salary"));
+//			emp.setDeptno(rs.getString("deptno"));
+//			emp.setTitle(rs.getString("title"));
+//			emp.setDname(rs.getString("dname"));
+//			empList.add(emp);
+//		}
+//		JDBCutil.closeResource(stmt, rs);
+//		return empList;
+//	}
+
+}
